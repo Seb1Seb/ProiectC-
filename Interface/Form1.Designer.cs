a30b93e7e5787832a052cf55a1bf23c1681d9549
Seb1Seb <148572929+Seb1Seb@users.noreply.github.com>
2025-04-01 09:12:57 +0300

    TextBox + Validari

--- Interface/Form1.Designer.cs
+++ Interface/Form1.Designer.cs
@@ -28,21 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(741, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introducere individ:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(741, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Introducere alimente:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(745, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(745, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox1.Location = new System.Drawing.Point(746, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox2.Location = new System.Drawing.Point(746, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox3.Location = new System.Drawing.Point(746, 261);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox4.Location = new System.Drawing.Point(745, 302);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox5.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox5.Location = new System.Drawing.Point(746, 347);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Click += new System.EventHandler(this.textBox5_Click);
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox6.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox6.Location = new System.Drawing.Point(745, 400);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Click += new System.EventHandler(this.textBox6_Click);
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.Leave += new System.EventHandler(this.textBox6_Leave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(987, 616);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -20,9 +20,11 @@ namespace Interface
 
         private Label lblNume;
         private Label lblPrenume;
+        private Label lblTipIndivid;
 
         private Label[] lblsNume;
         private Label[] lblsPrenume;
+        private Label[] lblsTipIndivid;
 
         private Label lbldenumire;
         private Label lbldata_exp;
@@ -40,8 +42,8 @@ namespace Interface
         private const int DIMENSIUNE_PAS_X = 20;
         private const int DIMENSIUNE_PAS_Y = 30;
 
-        private const string LocatieFisierIndivizi = "C:\\Users\\arhei\\Source\\Repos\\ProiectC-\\ProiectC#\\bin\\Debug\\Indivizi.txt";
-        private const string LocatieFisierAlimente = "C:\\Users\\arhei\\Source\\Repos\\ProiectC-\\ProiectC#\\bin\\Debug\\Alimente.txt";
+        private const string LocatieFisierIndivizi = "C:\\Users\\arhei\\source\\repos\\ProiectC#\\ProiectC#\\bin\\Debug\\Indivizi.txt";
+        private const string LocatieFisierAlimente = "C:\\Users\\arhei\\source\\repos\\ProiectC#\\ProiectC#\\bin\\Debug\\Alimente.txt";
 
         public Form1()
         {
@@ -72,6 +74,13 @@ namespace Interface
             lblPrenume.ForeColor = Color.DarkBlue;
             this.Controls.Add(lblPrenume);
 
+            lblTipIndivid = new Label();
+            lblTipIndivid.Width = LATIME_CONTROL;
+            lblTipIndivid.Text = "Tip Individ";
+            lblTipIndivid.Left = 220;
+            lblTipIndivid.ForeColor = Color.DarkBlue;
+            this.Controls.Add(lblTipIndivid);
+
             adminAliment = new AdministrareFisierAlimente(LocatieFisierAlimente);
             int nrAlimente = 0;
             Aliment[] alimente = adminAliment.GetAlimente(out nrAlimente);
@@ -79,30 +88,44 @@ namespace Interface
             lbldenumire = new Label();
             lbldenumire.Width = LATIME_CONTROL;
             lbldenumire.Text = "Denumire";
-            lbldenumire.Left = 220;
+            lbldenumire.Left = 320;
             lbldenumire.ForeColor = Color.DarkBlue;
             this.Controls.Add(lbldenumire);
 
             lbldata_exp = new Label();
             lbldata_exp.Width = LATIME_CONTROL;
             lbldata_exp.Text = "Data_exp";
-            lbldata_exp.Left = 320;
+            lbldata_exp.Left = 420;
             lbldata_exp.ForeColor = Color.DarkBlue;
             this.Controls.Add(lbldata_exp);
 
             lblcantitate = new Label();
             lblcantitate.Width = LATIME_CONTROL;
             lblcantitate.Text = "Cantitate";
-            lblcantitate.Left = 420;
+            lblcantitate.Left = 520;
             lblcantitate.ForeColor = Color.DarkBlue;
             this.Controls.Add(lblcantitate);
 
             lblgramaj = new Label();
             lblgramaj.Width = LATIME_CONTROL;
             lblgramaj.Text = "Gramaj";
-            lblgramaj.Left = 520;
+            lblgramaj.Left = 620;
             lblgramaj.ForeColor = Color.DarkBlue;
             this.Controls.Add(lblgramaj);
+
+            lbltipAliment = new Label();
+            lbltipAliment.Width = LATIME_CONTROL;
+            lbltipAliment.Text = "Tip Aliment";
+            lbltipAliment.Left = 720;
+            lbltipAliment.ForeColor = Color.DarkBlue;
+            this.Controls.Add(lbltipAliment);
+
+            textBox1.Text = "Nume";
+            textBox2.Text = "Prenume";
+            textBox3.Text = "Denumire";
+            textBox4.Text = "Data_exp";
+            textBox5.Text = "Cantitate";
+            textBox6.Text = "Gramaj";
         }
         private void AfiseazaIndivizi()
         {
@@ -110,6 +133,7 @@ namespace Interface
 
             lblsNume = new Label[nrIndivizi];
             lblsPrenume = new Label[nrIndivizi];
+            lblsTipIndivid = new Label[nrIndivizi];
 
             int i = 0;
             foreach (Individ individ in indivizi)
@@ -127,6 +151,14 @@ namespace Interface
                 lblsPrenume[i].Left = 120;
                 lblsPrenume[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsPrenume[i]);
+
+                lblsTipIndivid[i] = new Label();
+                lblsTipIndivid[i].Width = LATIME_CONTROL;
+                lblsTipIndivid[i].Text = individ.tipIndivid.ToString();
+                lblsTipIndivid[i].Left = 220;
+                lblsTipIndivid[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblsTipIndivid[i]);
+
                 i++;
             }
         }
@@ -143,6 +175,7 @@ namespace Interface
             lblsdata_exp = new Label[nrAlimente];
             lblscantitate = new Label[nrAlimente];
             lblsgramaj = new Label[nrAlimente];
+            lblstipAliment = new Label[nrAlimente];
 
             int i = 0;
             foreach (Aliment aliment in alimente)
@@ -150,28 +183,320 @@ namespace Interface
                 lblsdenumire[i] = new Label();
                 lblsdenumire[i].Width = LATIME_CONTROL;
                 lblsdenumire[i].Text = aliment.denumire;
-                lblsdenumire[i].Left = 220;
+                lblsdenumire[i].Left = 320;
                 lblsdenumire[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsdenumire[i]);
+
                 lblsdata_exp[i] = new Label();
                 lblsdata_exp[i].Width = LATIME_CONTROL;
                 lblsdata_exp[i].Text = aliment.data_exp;
-                lblsdata_exp[i].Left = 320;
+                lblsdata_exp[i].Left = 420;
                 lblsdata_exp[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsdata_exp[i]);
+
                 lblscantitate[i] = new Label();
                 lblscantitate[i].Width = LATIME_CONTROL;
                 lblscantitate[i].Text = aliment.cantitate.ToString();
-                lblscantitate[i].Left = 420;
+                lblscantitate[i].Left = 520;
                 lblscantitate[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblscantitate[i]);
+
                 lblsgramaj[i] = new Label();
                 lblsgramaj[i].Width = LATIME_CONTROL;
                 lblsgramaj[i].Text = aliment.gramaj.ToString();
-                lblsgramaj[i].Left = 520;
+                lblsgramaj[i].Left = 620;
                 lblsgramaj[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
                 this.Controls.Add(lblsgramaj[i]);
+
+                lblstipAliment[i] = new Label();
+                lblstipAliment[i].Width = LATIME_CONTROL;
+                lblstipAliment[i].Text = aliment.tipAliment.ToString();
+                lblstipAliment[i].Left = 720;
+                lblstipAliment[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblstipAliment[i]);
+
                 i++;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxValid(textBox1))
+            {
+                MessageBox.Show("Invalid data.");
+            }
+            else
+            if (textBoxValid(textBox2))
+            {
+                MessageBox.Show("Invalid data.");
+            }
+            else
+            if (textBox1.Text != "Nume" && textBox2.Text != "Prenume")
+            {
+                int nrIndivizi = 0;
+                Individ[] indivizi = adminIndivid.GetIndivizi(out nrIndivizi);
+
+                string numeBox = textBox1.Text;
+                string prenumeBox = textBox2.Text;
+                Individ individ = new Individ(numeBox, prenumeBox, 1, "Inalt");
+                adminIndivid.AddIndivid(individ, ref nrIndivizi);
+                AfiseazaIndivizi();
+
+                textBox1.Text = "Nume";
+                textBox1.ForeColor = System.Drawing.Color.Gray;
+                textBox2.Text = "Prenume";
+                textBox2.ForeColor = System.Drawing.Color.Gray;
+            }
+            if (textBoxValid(textBox1))
+            {
+                textBox1.Text = "Nume";
+                textBox1.ForeColor = System.Drawing.Color.Gray;
+            }
+            if (textBoxValid(textBox2))
+            {
+                textBox2.Text = "Prenume";
+                textBox2.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBoxValid(textBox3))
+            {
+                MessageBox.Show("Invalid data.");
+            }
+            else
+            if (textBoxValid(textBox4))
+            {
+                MessageBox.Show("Invalid data.");
+            }
+            else
+            if (textBox1.Text != "Denumire" && textBox2.Text != "Data_exp")
+            {
+                int nrAliment = 0;
+                Aliment[] alimente = adminAliment.GetAlimente(out nrAliment);
+
+                string denumireBox = textBox3.Text;
+                string data_expBox = textBox4.Text;
+                int cantitateBox = Convert.ToInt32(textBox5.Text);
+                float gramajBox = Convert.ToSingle(textBox6.Text);
+                Aliment aliment = new Aliment(denumireBox, data_expBox, cantitateBox, gramajBox, 1);
+                adminAliment.AddAliment(aliment, ref nrAliment);
+                AfiseazaAlimente();
+
+                textBox3.Text = "Denumire";
+                textBox3.ForeColor = System.Drawing.Color.Gray;
+                textBox4.Text = "Data_exp";
+                textBox4.ForeColor = System.Drawing.Color.Gray;
+            }
+            if (textBoxValid(textBox3))
+            {
+                textBox3.Text = "Denumire";
+                textBox3.ForeColor = System.Drawing.Color.Gray;
+            }
+            if (textBoxValid(textBox4))
+            {
+                textBox4.Text = "Data_exp";
+                textBox4.ForeColor = System.Drawing.Color.Gray;
+            }
+
+            textBox5.Text = "Cantitate";
+            textBox5.ForeColor = System.Drawing.Color.Gray;
+            textBox6.Text = "Gramaj";
+            textBox6.ForeColor = System.Drawing.Color.Gray;
+        }
+        private bool textBoxValid(TextBox textBox)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "[^a-zA-Z]"))
+            {
+                return true;
+            }
+            if (textBox.Text.Length > 12)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxValid(textBox1))
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+            else
+            {
+                if (textBox1.Text != "Nume")
+                    textBox1.ForeColor = Color.Black;
+                if (textBox1.Text == "Nume")
+                    textBox1.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxValid(textBox2))
+            {
+                textBox2.ForeColor = Color.Red;
+            }
+            else
+            {
+                if (textBox2.Text != "Prenume")
+                    textBox2.ForeColor = Color.Black;
+                if (textBox2.Text == "Prenume")
+                    textBox2.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxValid(textBox3))
+            {
+                textBox3.ForeColor = Color.Red;
+            }
+            else
+            {
+                if (textBox3.Text != "Denumire")
+                    textBox3.ForeColor = Color.Black;
+                if (textBox3.Text == "Denumire")
+                    textBox3.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox1.Text == "Nume")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox2.Text == "Prenume")
+            {
+                textBox2.Text = "";
+            }
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox3.Text == "Denumire")
+            {
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            textBox4.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox4.Text == "Data_exp")
+            {
+                textBox4.Text = "";
+            }
+        }
+
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox5.Text == "Cantitate")
+            {
+                textBox5.Text = "";
+            }
+        }
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            textBox6.ForeColor = System.Drawing.Color.Black;
+            // Only clear the text when the user starts typing
+            if (textBox6.Text == "Gramaj")
+            {
+                textBox6.Text = "";
+            }
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = "Nume";
+                textBox1.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Text = "Prenume";
+                textBox2.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                textBox3.Text = "Denumire";
+                textBox3.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox4.Text))
+            {
+                textBox4.Text = "Data_exp";
+                textBox4.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox5.Text))
+            {
+                textBox5.Text = "Cantitate";
+                textBox5.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void textBox6_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox6.Text))
+            {
+                textBox6.Text = "Gramaj";
+                textBox6.ForeColor = System.Drawing.Color.Gray;
             }
         }
     }
